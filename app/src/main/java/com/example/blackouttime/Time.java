73397e08a95224d478cbac40916551fefe2f16e1
dannnik4 <daniilperstnev2001@gmail.com>
2024-06-25 23:22:24 +0300

    Added Time.java

---
+++ app/src/main/java/com/example/blackouttime/Time.java
@@ -0,0 +1,8 @@
+package com.example.blackouttime;
+
+import android.widget.EditText;
+
+public class Time {
+    EditText startTimeEditText = rootView.findViewById(R.id.start_time);
+    EditText endTimeEditText = rootView.findViewById(R.id.end_time);
+}
