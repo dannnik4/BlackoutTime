d6697a6d9935e8eeec33064f2542d583f3ab6866
dannnik4 <daniilperstnev2001@gmail.com>
2024-06-26 23:23:59 +0300

    Updated Time.java

--- app/src/main/java/com/example/blackouttime/Time.java
+++ app/src/main/java/com/example/blackouttime/Time.java
@@ -3,6 +3,6 @@ package com.example.blackouttime;
 import android.widget.EditText;
 
 public class Time {
-    EditText startTimeEditText = rootView.findViewById(R.id.start_time);
-    EditText endTimeEditText = rootView.findViewById(R.id.end_time);
+//    EditText startTimeEditText = rootView.findViewById(R.id.start_time);
+//    EditText endTimeEditText = rootView.findViewById(R.id.end_time);
 }
