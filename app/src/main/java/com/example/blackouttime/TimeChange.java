ced90bf01b41e9d40273d35e6068583feaa87724
dannnik4 <daniilperstnev2001@gmail.com>
2024-06-29 20:43:48 +0300

    Updated TimeChange.java

--- app/src/main/java/com/example/blackouttime/TimeChange.java
+++ app/src/main/java/com/example/blackouttime/TimeChange.java
@@ -1,9 +1,7 @@
-package com.example.myapp;
+package com.example.blackouttime;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,25 +9,27 @@ import android.widget.EditText;
 import android.text.Editable;
 import android.text.TextWatcher;
 
-public class TimeChange extends Fragment {
+import com.example.blackouttime.Time;
+
+public class TimeChange extends Time {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    public static PlaceholderFragment newInstance(int sectionNumber) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
+    public static TimeChange newInstance(int sectionNumber) {
+        TimeChange fragment = new TimeChange();
         Bundle args = new Bundle();
         args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public PlaceholderFragment() {
+    public TimeChange() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.time, container, false);
 
         EditText startTimeEditText = rootView.findViewById(R.id.start_time);
         EditText endTimeEditText = rootView.findViewById(R.id.end_time);
