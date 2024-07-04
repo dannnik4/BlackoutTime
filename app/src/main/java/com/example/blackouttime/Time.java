5ac28fde937f17a0cd9fbe17d2bc01f75baf894b
dannnik4 <daniilperstnev2001@gmail.com>
2024-07-04 23:11:34 +0300

    Updated Time.java

--- app/src/main/java/com/example/blackouttime/Time.java
+++ app/src/main/java/com/example/blackouttime/Time.java
@@ -1,8 +1,24 @@
 package com.example.blackouttime;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-public class Time {
-    EditText startTimeEditText = rootView.findViewById(R.id.start_time);
-    EditText endTimeEditText = rootView.findViewById(R.id.end_time);
+public class Time extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_time, container, false);
+
+        EditText startTimeEditText = rootView.findViewById(R.id.start_time);
+        EditText endTimeEditText = rootView.findViewById(R.id.end_time);
+
+        return rootView;
+    }
 }
