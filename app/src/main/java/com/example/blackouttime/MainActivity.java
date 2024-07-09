31043289d73d2db56d948c96bef286731d7d6a91
dannnik4 <daniilperstnev2001@gmail.com>
2024-07-09 18:53:18 +0300

    Updated MainActivity.java

--- app/src/main/java/com/example/blackouttime/MainActivity.java
+++ app/src/main/java/com/example/blackouttime/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.blackouttime;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 }
