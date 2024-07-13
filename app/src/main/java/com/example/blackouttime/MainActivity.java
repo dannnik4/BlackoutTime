3fde6fec9b0f1bdbe6a976f3cac6bca1cbebc4f0
dannnik4 <daniilperstnev2001@gmail.com>
2024-07-13 22:44:31 +0300

    Updated MainActivity.java

--- app/src/main/java/com/example/blackouttime/MainActivity.java
+++ app/src/main/java/com/example/blackouttime/MainActivity.java
@@ -1,7 +1,47 @@
 package com.example.blackouttime;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.todaytab:
+                        fragment = new TodayTab();
+                        break;
+                    case R.id.tommorowtab:
+                        fragment = new TommorowTab();
+                        break;
+                    default:
+                        return false;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+                return true;
+            }
+        });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new TodayTab());
+        transaction.commit();
+    }
 
 }
