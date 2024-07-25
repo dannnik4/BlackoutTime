82a3ad181243ac83ceaeefbd9e5f76be6d8e2346
dannnik4 <daniilperstnev2001@gmail.com>
2024-07-25 19:13:53 +0300

    Updated TodayTab.java

--- app/src/main/java/com/example/blackouttime/TodayTab.java
+++ app/src/main/java/com/example/blackouttime/TodayTab.java
@@ -1,6 +1,22 @@
 package com.example.blackouttime;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 import androidx.fragment.app.Fragment;
 
 public class TodayTab extends Fragment {
+
+    private LinearLayout container;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+        this.container = view.findViewById(R.id.container);
+
+        return view;
+    }
 }
