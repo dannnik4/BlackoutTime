cd2b8144c67dafd006e9c307d0f36b13f9ee92a2
dannnik4 <daniilperstnev2001@gmail.com>
2024-08-27 18:40:02 +0300

    Updated TodayTab.java

--- app/src/main/java/com/example/blackouttime/TodayTab.java
+++ app/src/main/java/com/example/blackouttime/TodayTab.java
@@ -1,23 +1,14 @@
 package com.example.blackouttime;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TodayTab extends Fragment {
 
