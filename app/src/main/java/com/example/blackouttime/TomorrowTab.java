de4ecd02fe7fd06d631473dab3f56d6bede5e6e4
dannnik4 <daniilperstnev2001@gmail.com>
2024-08-28 22:40:50 +0300

    Updated TomorrowTab.java

--- app/src/main/java/com/example/blackouttime/TomorrowTab.java
+++ app/src/main/java/com/example/blackouttime/TomorrowTab.java
@@ -1,16 +1,9 @@
 package com.example.blackouttime;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
