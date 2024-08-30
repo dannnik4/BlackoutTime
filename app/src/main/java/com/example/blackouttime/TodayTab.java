c818357edd1fe824bca2cd516faa2a0d2a23f07a
dannnik4 <daniilperstnev2001@gmail.com>
2024-08-30 23:44:24 +0300

    Updated TodayTab.java

--- app/src/main/java/com/example/blackouttime/TodayTab.java
+++ app/src/main/java/com/example/blackouttime/TodayTab.java
@@ -14,16 +14,16 @@ public class TodayTab extends Fragment {
 
     private EditText startTimeEditText, endTimeEditText, intenseStartTimeEditText, intenseEndTimeEditText;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_layout, container, false);
-
-        startTimeEditText = rootView.findViewById(R.id.start_time);
-        endTimeEditText = rootView.findViewById(R.id.end_time);
-        intenseStartTimeEditText = rootView.findViewById(R.id.intense_start_time);
-        intenseEndTimeEditText = rootView.findViewById(R.id.intense_end_time);
-
-        return rootView;
-    }
-}
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.fragment_layout, container, false);
+//
+//        startTimeEditText = rootView.findViewById(R.id.start_time);
+//        endTimeEditText = rootView.findViewById(R.id.end_time);
+//        intenseStartTimeEditText = rootView.findViewById(R.id.intense_start_time);
+//        intenseEndTimeEditText = rootView.findViewById(R.id.intense_end_time);
+//
+//        return rootView;
+//    }
+//}
