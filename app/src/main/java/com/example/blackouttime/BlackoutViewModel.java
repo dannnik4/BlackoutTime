36bf640da98242097a8531bdc00653ed35a174b9
dannnik4 <daniilperstnev2001@gmail.com>
2024-09-23 21:15:50 +0300

    Updated BlackoutViewModel.java

--- app/src/main/java/com/example/blackouttime/BlackoutViewModel.java
+++ app/src/main/java/com/example/blackouttime/BlackoutViewModel.java
@@ -3,4 +3,12 @@ package com.example.blackouttime;
 import androidx.lifecycle.ViewModel;
 
 public class BlackoutViewModel extends ViewModel {
+    public String startTime = "";
+    public String endTime = "";
+    public String intenseStartTime = "";
+    public String intenseEndTime = "";
+    public boolean noBlackoutChecked = false;
+    public boolean allDayChecked = false;
+    public boolean noIntenseBlackoutChecked = false;
+    public boolean intenseAllDayChecked = false;
 }
